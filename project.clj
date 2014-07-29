7a12bb429aa9d07e5fea30975f655655fecdd1fc
Zack Maril <zmaril@sunlightfoundation.com>
2014-07-29 17:12:33 +0000

    Increasing heap size for both ECHELON and datomic.

--- project.clj
+++ project.clj
@@ -9,5 +9,8 @@
                  [me.raynes/fs "1.4.4"]
                  [instaparse "1.3.2"]
                  [org.jordanlewis/data.union-find "0.1.0"]
-                 [clj-time "0.7.0"]]
+                 [clj-time "0.7.0"]
+                 [environ "0.5.0"]]
+  :plugins [[lein-environ "0.5.0"]]
+  :jvm-opts ["-Xmx8g","-Xms6g","-server"]
   :main echelon.core)
